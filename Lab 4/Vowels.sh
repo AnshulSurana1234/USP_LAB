045681fc0335c81c5fd13ac62b23fff534ac4105
AnshulSurana1234 <71318982+AnshulSurana1234@users.noreply.github.com>
2022-01-08 10:33:48 +0530

    Update Vowels.sh

--- Lab 4/Vowels.sh
+++ Lab 4/Vowels.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+
+echo "enter the string"
+read str
+count=0
+
+for $i in str
+do
+if [ $i=='a' -o $i=='e' -o $i=='i' -o $i=='o' -o $i=='u' ]
+then
+count++
+done
+
+echo "The number of vowels in the string is $count"
